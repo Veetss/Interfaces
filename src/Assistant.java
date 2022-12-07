34fcf34b8a7e2348ff686d9f1c3ead5fc8bf9083
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:21:08 +0100

    Update Assistant.java

--- src/Assistant.java
+++ src/Assistant.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Assistant {
-}
+public class Assistant extends CollegePerson implements TeachingPerson, LearningPerson {
+
+    String willBeAPhD;
+
+    public void teachToOtherPeople() {
+        System.out.println("The assistant " + name + " " + surname + ", college id: " + collegeId + ", can be a teaching person that teach to other people and " + willBeAPhD);
+    }
+
+    public void studyAtHome() {
+        System.out.println("The assistant " + name + " " + surname + ", college id: " + collegeId + ", can be a learning person that study at home and " + willBeAPhD);
+    }
+
+
+    /**
+     *
+     * @param name
+     * @param surname
+     * @param collegeId
+     * @param willBeAPhD
+     */
+    Assistant(String name, String surname, int collegeId, String willBeAPhD) {
+        this.name = name;
+        this.surname = surname;
+        this.collegeId = collegeId;
+        this.willBeAPhD = willBeAPhD;
+  
