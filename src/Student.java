993a4184c55a5164d23e542cf256ba2ac9f9148c
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:23:33 +0100

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Student {
+public class Student extends CollegePerson implements LearningPerson {
+
+    int academicYear;
+
+    public void studyAtHome() {
+        System.out.println("The student " + name + " " + surname + ", college Id: " + collegeId + ", is a learning person from academic year: " + academicYear + " that study at home.");
+    }
+
+    /**
+     *
+     * @param name
+     * @param surname
+     * @param collegeId
+     * @param academicYear
+     */
+    Student(String name, String surname, int collegeId, int academicYear) {
+        this.name = name;
+        this.surname = surname;
+        this.collegeId = collegeId;
+        this.academicYear = academicYear;
+    }
 }
