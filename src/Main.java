149acda0f1bd85fc8b6741c4e45e81acd84a9f3d
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:22:35 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
+
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Student pino = new Student("Pino", "Daniele", 5698, 3);
+        Professor marco = new Professor("Marco", "Biondi", 1240, "literature");
+        Assistant marta = new Assistant("Marta", "Micheli", 3022, "Is a PhD");
+
+        pino.goToCollege();
+        marco.goToCollege();
+        marta.goToCollege();
+
+        pino.studyAtHome();
+        marco.teachToOtherPeople();
+        marta.studyAtHome();
+        marta.teachToOtherPeople();
     }
 }
