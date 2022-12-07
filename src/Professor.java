ffbcc53dbf9e484d5d27be7717e27a870fcfd4e7
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:23:05 +0100

    Update Professor.java

--- src/Professor.java
+++ src/Professor.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Professor {
+public class Professor extends CollegePerson implements TeachingPerson {
+
+    String subject;
+
+    public void teachToOtherPeople() {
+        System.out.println("The " + subject + " " + "professor " + name + " " + surname + ", id: " + collegeId + ", is a teaching person who teach to other people.");
+    }
+
+    /**
+     *
+     * @param name
+     * @param surname
+     * @param Id
+     * @param subject
+     */
+    Professor(String name, String surname, int Id, String subject) {
+        this.name = name;
+        this.surname = surname;
+        this.collegeId = Id;
+        this.subject = subject;
+    }
 }
